7a87ba06825c983f1a56ac52a08d57b2f6ee1722
Abdulla Al Mued <abdullaalmuid101@gamil.com>
2022-05-19 23:37:27 +0600

    abc

---
+++ app/src/main/java/com/example/cricketpara/fullScore.java
@@ -0,0 +1,14 @@
+package com.example.cricketpara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fullScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_score);
+    }
+}
---
+++ app/src/main/java/com/example/cricketpara/scoreBoard.java
@@ -0,0 +1,14 @@
+package com.example.cricketpara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scoreBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_boad);
+    }
+}
