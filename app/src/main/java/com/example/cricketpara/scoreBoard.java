efb3779860e7318a80d4b6baab142dc0a45dcd8a
mued <almued101@gmail.com>
2022-05-29 22:59:38 +0600

    ui complete

--- app/src/main/java/com/example/cricketpara/scoreBoard.java
+++ app/src/main/java/com/example/cricketpara/scoreBoard.java
@@ -2,13 +2,30 @@ package com.example.cricketpara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class scoreBoard extends AppCompatActivity {
+
+    Button full_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_boad);
+
+        full_score = findViewById(R.id.full_score);
+
+
+        full_score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), fullScore.class));
+            }
+        });
+
+
     }
 }
